7d9ab00821cb51c4fc644406f25f8e62e05dded0
Szczepan Wikar <szczepanwikar@gmail.com>
2024-10-13 16:24:01 +0200

    AccountDeletedHandler

---
+++ Backend/ReadModel/User/Handlers/AccountDeletedHandler.cs
@@ -0,0 +1,36 @@
+﻿using Core.Common.Projections;
+using Core.User.Events;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReadModel.User.Handlers
+{
+    public sealed class AccountDeletedHandler : IEventNotificationHandler<AccountDeleted>
+    {
+        private readonly ApplicationContext _context;
+
+        public AccountDeletedHandler(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(
+            EventNotification<AccountDeleted> notification,
+            CancellationToken cancellationToken
+        )
+        {
+            var user = await _context
+                .Set<UserEntity>()
+                .Where(e => e.Id == notification.Event.Id)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (user is null)
+            {
+                return;
+            }
+
+            user.Delete();
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
--- Backend/ReadModel/User/UserEntity.cs
+++ Backend/ReadModel/User/UserEntity.cs
@@ -1,4 +1,5 @@
-﻿using Core.User;
+﻿using Azure.Identity;
+using Core.User;
 using ReadModel.Expense;
 using ReadModel.Group;
 
@@ -11,11 +12,21 @@ namespace ReadModel.User
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public UserStatus Status { get; set; }
+        public bool Deleted { get; set; } = false;
         public ICollection<UserGroupEntity> UserGroups { get; set; }
         public ICollection<ExpenseEntity> Expenses { get; set; }
         public ICollection<ExpenseDeptorEntity> Depts { get; set; }
         public ICollection<GroupEntity> OwnedGroups { get; set; }
         public ICollection<BalanceEntity> CreditBalances { get; set; }
         public ICollection<BalanceEntity> DeptBalances { get; set; }
+
+        public void Delete() 
+        {
+            Username = "USER_DELETED";
+            Email = string.Empty;
+            Password = string.Empty;
+            Status = UserStatus.Blocked;
+            Deleted = false;
+        }
     }
 }
