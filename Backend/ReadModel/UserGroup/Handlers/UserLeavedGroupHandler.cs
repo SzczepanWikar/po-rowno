084a3e53374727032e48c424e9686f84a104bc94
Szczepan Wikar <szczepanwikar@gmail.com>
2024-10-15 21:03:13 +0200

    UserLeavedGroupHandler

---
+++ Backend/ReadModel/UserGroup/Handlers/UserLeavedGroupHandler.cs
@@ -0,0 +1,38 @@
+﻿using Core.Common.Projections;
+using Core.UserGroupEvents;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReadModel.UserGroup.Handlers
+{
+    public sealed class UserLeavedGroupHandler : IEventNotificationHandler<UserLeavedGroup>
+    {
+        private readonly ApplicationContext _context;
+
+        public UserLeavedGroupHandler(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(EventNotification<UserLeavedGroup> notification, CancellationToken cancellationToken)
+        {
+            var @event = notification.Event;
+            var userGroup = await _context
+                .Set<UserGroupEntity>()
+                .Where(e =>
+                    e.GroupId == @event.GroupId
+                    && e.UserId == @event.UserId
+                    && e.Status == UserGroupStatus.Active
+                )
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (userGroup is null)
+            {
+                return;
+            }
+
+            userGroup.Status = UserGroupStatus.Leaved;
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
