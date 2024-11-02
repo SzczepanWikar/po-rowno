b00a0417adb8e83949710b12d1edf89d1026ec25
Szczepan Wikar <szczepanwikar@gmail.com>
2024-11-02 18:23:15 +0100

    refactor move group data to service

--- po-rowno-app/src/app/groups/group-details/group-details.page.ts
+++ po-rowno-app/src/app/groups/group-details/group-details.page.ts
@@ -28,13 +28,10 @@ export class GroupDetailsPage implements OnInit, OnDestroy {
   protected loading$: Subject<boolean> = new BehaviorSubject(true);
   protected destroy$: Subject<void> = new Subject();
 
-  protected group?: Group;
-  protected balances: BalanceUser[] = [];
-  protected currencySymbol: string = '';
   protected currentSegment: SegmentValue = 'details';
 
   constructor(
-    private readonly groupService: GroupDetailsService,
+    protected readonly groupService: GroupDetailsService,
     private readonly route: ActivatedRoute,
     private readonly router: Router,
     private readonly modalCtrl: ModalController,
@@ -46,10 +43,7 @@ export class GroupDetailsPage implements OnInit, OnDestroy {
         .getGroup(e['id'])
         .pipe(takeUntil(this.destroy$))
         .subscribe({
-          next: (e) => {
-            this.group = e;
-            this.currencySymbol = getCurrencySymbol(e.currency);
-            this.balances = this.mapBalances(e.balances);
+          next: () => {
             this.loading$.next(false);
           },
           error: () => {
@@ -74,47 +68,4 @@ export class GroupDetailsPage implements OnInit, OnDestroy {
     this.destroy$.next();
     this.destroy$.complete();
   }
-
-  private mapBalances(balances?: Balance[]): BalanceUser[] {
-    if (!balances?.length) {
-      return [];
-    }
-
-    const res: BalanceUser[] = [];
-    const users = new Map(
-      this.group?.userGroups?.flatMap((e) =>
-        e.user ? [[e.userId, e.user]] : [],
-      ),
-    );
-
-    for (const balance of balances) {
-      if (balance.balance <= 0) {
-        continue;
-      }
-
-      const payer = users.get(balance.payerId);
-      if (!payer) {
-        continue;
-      }
-
-      const deptor = users.get(balance.deptorId);
-      if (!deptor) {
-        continue;
-      }
-
-      res.push({
-        balance,
-        payer,
-        deptor,
-      });
-    }
-
-    return res;
-  }
-}
-
-interface BalanceUser {
-  balance: Balance;
-  payer: User;
-  deptor: User;
 }
--- po-rowno-app/src/app/groups/group-details/group-details.service.ts
+++ po-rowno-app/src/app/groups/group-details/group-details.service.ts
@@ -2,11 +2,22 @@ import { Injectable } from '@angular/core';
 import { GroupService } from '../../_services/group/group.service';
 import { Group } from 'src/app/_common/models/group';
 import { map } from 'rxjs';
+import { Balance } from 'src/app/_common/models/balance';
+import { User } from 'src/app/_common/models/user';
+import { getCurrencySymbol } from 'src/app/_common/helpers/get-currency-symbol';
+
+type BalanceUser = {
+  balance: Balance;
+  payer: User;
+  deptor: User;
+};
 
 @Injectable()
 export class GroupDetailsService {
   group?: Group;
-  groupId?: string;
+  groupId: string = '';
+  balances: BalanceUser[] = [];
+  currencySymbol: string = '';
 
   constructor(private readonly groupService: GroupService) {}
 
@@ -15,6 +26,8 @@ export class GroupDetailsService {
     return this.groupService.getOne(id).pipe(
       map((e) => {
         this.group = e;
+        this.balances = this.mapBalances(e.balances);
+        this.currencySymbol = getCurrencySymbol(e.currency);
 
         return e;
       }),
@@ -22,10 +35,45 @@ export class GroupDetailsService {
   }
 
   refreshJoinCode(validTo: Date) {
-    return this.groupService
-      .refreshJoinCode(this.groupId ?? '', validTo)
-      .pipe(() => {
-        return this.getGroup(this.groupId ?? '');
+    return this.groupService.refreshJoinCode(this.groupId, validTo).pipe(() => {
+      return this.getGroup(this.groupId);
+    });
+  }
+
+  private mapBalances(balances?: Balance[]): BalanceUser[] {
+    if (!balances?.length) {
+      return [];
+    }
+
+    const res: BalanceUser[] = [];
+    const users = new Map(
+      this.group?.userGroups?.flatMap((e) =>
+        e.user ? [[e.userId, e.user]] : [],
+      ),
+    );
+
+    for (const balance of balances) {
+      if (balance.balance <= 0) {
+        continue;
+      }
+
+      const payer = users.get(balance.payerId);
+      if (!payer) {
+        continue;
+      }
+
+      const deptor = users.get(balance.deptorId);
+      if (!deptor) {
+        continue;
+      }
+
+      res.push({
+        balance,
+        payer,
+        deptor,
       });
+    }
+
+    return res;
   }
 }
